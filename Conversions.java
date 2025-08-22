8476e66e4ee979d01a5f17736993111f2d5eaaf9
SkAlex1 <akspuhler01@bvsd.org>
2025-08-22 20:48:05 +0000

    Added rest of conversions and finished project

--- Conversions.java
+++ Conversions.java
@@ -19,6 +19,8 @@ public class Conversions
         System.out.println("6. Milliliters to Ounces");
         System.out.println("7. Meters to kiloMeters");
         System.out.println("8. Kilometers to meters");
+        System.out.println("9. Roblox Studs to meters");
+        System.out.println("10. Meters to roblox Studs");
         //Add 2 more conversions, each both ways, to add choices 7, 8, 9, and 10.
         //You can add even more if you would like.
 
@@ -33,14 +35,92 @@ public class Conversions
             double fahrenheit = celsius * (9.0 / 5.0) + 32;
             System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
         }
-        if (selection == 2)
+        if (selection == 2) // converts fahrenheit to celcius
         {
             System.out.println("Enter fahrenheit: ");
             double fahrenheit = keyboard.nextDouble();
             keyboard.nextLine();
-            double celcius = fahrenheit/ (9.0/5.0) + 32
-            System.out.println(fahrenheit + " degrees fahrenheit is " +  celcius " degrees celcius")
+            double celcius = fahrenheit/ (9.0/5.0) - 32;
+            System.out.println(fahrenheit + " degrees fahrenheit is " +  celcius + " degrees celcius");
         }
+
+        if (selection == 3)  // converts feet to meters. 
+        {
+            System.out.println("Enter Feet: ");
+            double feet = keyboard.nextDouble();
+            keyboard.nextLine();
+            double meters = feet * (0.304);
+            System.out.println(feet + " feet is " + meters + " meters");
+        }
+
+        if (selection == 5)
+        {
+            System.out.println("Enter ounces: ");
+            double ounces =  keyboard.nextDouble();
+            keyboard.nextLine();
+            double millimeters = ounces / (29.57);
+            System.out.println(ounces + " ounces is " + millimeters + " millimeters");
+
+
+        }
+
+        if (selection == 4)
+        {
+            System.out.println("Enter meters: ");
+            double meters =  keyboard.nextDouble();
+            keyboard.nextLine();
+            double feet = meters * (3.04);
+            System.out.println(meters + " Meters is " + feet + " Feet");
+
+
+        }
+
+         if (selection == 6)
+        {
+            System.out.println("Enter millimeters: ");
+            double meters =  keyboard.nextDouble();
+            keyboard.nextLine();
+            double feet = meters / (29.57);
+            System.out.println(meters + " millimeters is " + feet + " ounces");
+        }
+
+        if (selection == 7)
+        {
+            System.out.println("Enter Meters: ");
+            double meters =  keyboard.nextDouble();
+            keyboard.nextLine();
+            double kilometers = meters / (1000.00);
+            System.out.println(meters + " meters " + kilometers + " kilometers");
+        }
+
+        if (selection == 8)
+        {
+            System.out.println("Enter KiloMeters: ");
+            double kilometers =  keyboard.nextDouble();
+            keyboard.nextLine();
+            double meters = kilometers * (1000.00);
+            System.out.println(kilometers + " kilometers " + meters + " kilometers");
+        }
+
+        if (selection == 9)
+        {
+            System.out.println("Enter Studs: ");
+            double Studs =  keyboard.nextDouble();
+            keyboard.nextLine();
+            double meters = Studs * (0.28);
+            System.out.println(Studs + " Studs " + meters + " Meters");
+        }
+
+         if (selection == 10)
+        {
+            System.out.println("Enter Meters: ");
+            double meters =  keyboard.nextDouble();
+            keyboard.nextLine();
+            double studs  = meters / (0.28);
+            System.out.println(meters + " Meters " + studs + " studs");
+        }
+
+
         //add more if statements here.
     }
 }
